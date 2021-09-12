173caf0cd2425b3d5384199113e9484002d74a8e
Range Quest <range.quest@gmail.com>
2021-09-12 09:12:32 +0530

    Used logger.log instead console.log.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const logger = require('./logger')
 
 require('./startup/routes')(app)
 
+throw new Error('Oops!')
+
 const port = process.env.PORT || 3000
 const server = app.listen(port, () =>
-  console.log(`Server is listening on port ${port}...` )
+  logger.log({ level: 'info', message: `Server is listening on port ${port}...` })
 )
 module.exports = server
